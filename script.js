058731889ac3c7c89942b86a391f868146955c41
huanamigo <arturfurtak5@gmail.com>
2022-08-14 09:37:08 +0200

    language change

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-const ball = document.querySelector("img");
-const input = document.querySelector("input");
-const answerP = document.querySelector(".answer");
-const errorP = document.querySelector(".error");
+const ball = document.querySelector('img');
+const input = document.querySelector('input');
+const answerP = document.querySelector('.answer');
+const errorP = document.querySelector('.error');
 
 const answersArr = [
-  "Tak!",
-  "Zdecydowanie tak!",
-  "Nie",
-  "Zdecydowanie nie!",
-  "Ciężko powiedzieć..",
-  "Żartujesz sobie?",
-  "Nie chcesz znać odpowiedzi...",
-  "Zapomnij!",
-  "Prawie na pewno tak",
-  "Sam powinieneś wiedzieć!",
-  "Wszystko mówi że tak więc zrób na odwrót!",
+  'Yes!',
+  'Definitely yes!',
+  'Not',
+  'Definitely not!',
+  'Hard to say..',
+  "You're kidding?",
+  "You don't want to know the answer ...",
+  'Forget!',
+  'Almost certainly yes',
+  'You should know it yourself!',
+  'Everything says so do the other way around!',
 ];
 
 const shakeBall = () => {
-  ball.classList.add("shake-animaton");
+  ball.classList.add('shake-animaton');
   setTimeout(checkInput, 1000);
 };
 
-ball.addEventListener("click", () => {
-  if (input.value == "") {
-    errorP.innerText = "Musisz zadać pytanie!";
-    answerP.innerText = "";
+ball.addEventListener('click', () => {
+  if (input.value == '') {
+    errorP.innerText = 'You have to ask a question!';
+    answerP.innerText = '';
   } else {
-    if (input.value.charAt(input.value.length - 1) == "?") {
+    if (input.value.charAt(input.value.length - 1) == '?') {
       let number = Math.floor(Math.random() * answersArr.length);
-      errorP.innerText = "";
+      errorP.innerText = '';
       answerP.innerText = answersArr[number];
       shakeBall();
     } else {
-      errorP.innerText = "Pytanie musi kończyć się pytajnikiem!";
-      answerP.innerText = "";
+      errorP.innerText = 'The question must end with a question mark!';
+      answerP.innerText = '';
     }
   }
 });
